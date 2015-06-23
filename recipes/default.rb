bc05e0efe13f0a7583742028a6bd1ffac869a3a4
sawanoboly <sawanoboriyu@higanworks.com>
2015-06-23 15:03:36 +0900

    Hash ja nai yona-.

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,7 +13,7 @@ end
 bash 'install_fail2ban' do
   action :nothing
   flags '-ex'
-  environment { "PATH" => 'opt/local/bin:/opt/local/sbin' }
+  environment "PATH" => 'opt/local/bin:/opt/local/sbin'
   cwd "#{Chef::Config[:file_cache_path]}/fail2ban_smartos"
   code <<-EOH
   python setup.py install
